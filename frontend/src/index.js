b7caa2d545dcb5f00712ba17ad2b177411207c61
Senthil NSenthil <senthiln0281@gmail.com>
2026-04-07 17:00:30 +0000

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // This links to your existing App.js
+import App from './App'; 
+// 1. Add these two important imports
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './authConfig';
+
+// 2. Initialize the MSAL instance with your config
+const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 3. Wrap your App in the Provider */}
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
